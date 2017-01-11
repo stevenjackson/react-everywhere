836896860ec6a3104b0b020cbafad0497f2ae4c2
Steve Jackson <steve@testdouble.com>
2017-01-11 14:51:40 -0500

    Add toggle component for filtering

--- src/SessionList.js
+++ src/SessionList.js
@@ -3,6 +3,14 @@ import { connect } from 'react-redux'
 import Session from './Session'
 
 const URL = './session.json'
+
+const Toggle = (props)  => {
+  const { toggled, onToggle, text } = props
+  return <div className={toggled ? 'toggled' : ''}>
+    <a href="#" onClick={onToggle}>{text}</a>
+  </div>
+}
+
 class SessionList extends Component {
   constructor(props) {
     super(props);
@@ -11,9 +19,17 @@ class SessionList extends Component {
     //wha???
     fetch(URL).then(r => r.json()).then((sessions) => fetchedSessions(sessions))
   }
+  filterSessions(sessionName) {
+    console.log(sessionName);
+  }
   render() {
     const { sessions } = this.props
+    const filter = (sessionName) => this.filterSessions.bind(this, sessionName)
     return <div>
+      <div className="toggles">
+        <Toggle text="Pre-Compiler" onToggle={filter("Pre-Compiler")}/>
+        <Toggle text="KidzMash" onToggle={filter("Kidz Mash")}/>
+      </div>
       { sessions.map(s => <Session key={s.Id} {...s} />) }
     </div>
   }
